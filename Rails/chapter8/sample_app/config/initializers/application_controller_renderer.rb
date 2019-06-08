8e41fbfe9aaa0665b53d7ca3baf0587909f7b035
宮岡　逹野 <tatsuya@miyaokatachiyanoMacBook-Air.local>
2019-06-08 20:11:33 +0900

    [Finish] 3-8

---
+++ Rails/chapter8/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e912998ede9464c2e839c8a9218f33550f3c8fde3e76c65e4c451087511a0d33d18b33f54d1231f6c857be115bdf53bd523d418b3b4f2a69982ac897aae1c1e5'
